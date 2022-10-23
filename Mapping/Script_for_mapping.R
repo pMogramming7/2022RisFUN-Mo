9e6703cf1505b8d36dfc42b6ba54d622ec4ef908
pMogramming7 <momail710@gmail.com>
2022-10-24 00:33:31 +0800

    Homework_done_10/24

---
+++ Mapping/Script_for_mapping.R
@@ -0,0 +1,15 @@
+# MAPPING
+library(maptools) #Basic Map
+library(raster) #for GADM
+library(rgdal) #for gpx and other "geospatial" data
+#Using ggplot2
+library(tidyverse)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
+#For GBIF DATA
+#library(rgbif)
+#For distribution map
+#library(mapr)
+library(marmap) #Bathymetric map等深等壓線圖
+library(leaflet) #leaflet in webpage or markdown
